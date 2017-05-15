63aa272b9003a4b98e501f7c32483d8417275569
Gabriel Eduardo Duarte Vega <geduartev@gmail.com>
2017-05-14 21:22:47 -0500

    Add project files.

---
+++ AndroidApp/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidApp
+{
+    [Activity(Label = "AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            var Validator = new PCLProyect.AppValidator(new AndroidDialog(this));
+
+            Validator.Device = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+            Validator.Email = "email";
+            Validator.Password = "password";
+
+            Validator.Validate();
+            
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
